ccc57bd1beb77394f04ba4061349fc840f8fa586
Nick Walker <nick@nickwalker.us>
2024-06-22 23:17:10 -0700

    Refactor track script
    Rely on precomputed detections and output results to a separate directory
    Move responsibility for displaying images out of the tracker itself

---
+++ track.py
@@ -0,0 +1,209 @@
+#!/usr/bin/env python
+import argparse
+import pathlib
+
+import numpy as np
+from pathlib import Path
+
+import torch
+import yaml
+from types import SimpleNamespace
+
+from timecode import Timecode
+from ultralytics.engine.results import Boxes
+
+from visual_race_timing.annotations import save_txt_annotation, load_annotations
+from visual_race_timing.drawing import draw_annotation
+
+from visual_race_timing.geometry import side_of_line
+from visual_race_timing.video import get_video_height_width, crop_videos
+from visual_race_timing.tracker import RaceTracker
+
+from visual_race_timing.video_player import VideoPlayer, DisplayWindow
+
+
+class RunnerState:
+    BEFORE_LINE_LEFT = 0
+    BEFORE_LINE_RIGHT = 1
+    AFTER_LINE = 2
+    NOT_VISIBLE = 3
+
+
+def update_crossings(result, finish_line_p0, finish_line_p1, crossings):
+    # FIXME
+    for i, (box, kp) in enumerate(zip(result.boxes, result.keypoints)):
+        if box.id is None:
+            # Tracker hasn't ID'd this box yet
+            continue
+        if kp is None:
+            # Must not be running a pose model
+            continue
+        runner_id = int(box.id.cpu().numpy()[0])
+        kpts = kp.xy.squeeze().cpu().numpy()[15:]
+        if not np.all(kpts):
+            continue
+        sides = side_of_line(finish_line_p0, finish_line_p1, kpts)
+        if sides[0] == 0 and sides[1] == 0:
+            state = RunnerState.BEFORE_LINE_LEFT
+        elif sides[0] == 1 and sides[1] == 1:
+            state = RunnerState.BEFORE_LINE_RIGHT
+        else:
+            state = RunnerState.AFTER_LINE
+
+        if runner_id not in runners.keys():
+            runners[runner_id] = state
+            if state == RunnerState.AFTER_LINE:
+                crossings[i] = True
+        elif runners[runner_id] == RunnerState.NOT_VISIBLE:
+            runners[runner_id] = state
+            if state == RunnerState.AFTER_LINE:
+                crossings[i] = True
+        elif runners[runner_id] == RunnerState.BEFORE_LINE_LEFT and (
+                state == RunnerState.BEFORE_LINE_RIGHT or state == RunnerState.AFTER_LINE):
+            runners[runner_id] = RunnerState.AFTER_LINE
+            crossings[i] = True
+        elif runners[runner_id] == RunnerState.BEFORE_LINE_RIGHT and (
+                state == RunnerState.BEFORE_LINE_LEFT or state == RunnerState.AFTER_LINE):
+            runners[runner_id] = RunnerState.AFTER_LINE
+            crossings[i] = True
+
+
+@torch.no_grad()
+def run(args):
+    display_window = DisplayWindow("Race Timing")
+    display_window.start()
+
+    # Load race configuration from yaml
+    race_config = args.project / 'config.yaml'
+    with open(race_config, "r") as f:
+        race_config = yaml.load(f.read(), Loader=yaml.FullLoader)
+
+    tracker_config = args.project / 'tracker_config.yaml'
+    with open(tracker_config, "r") as f:
+        cfg = yaml.load(f.read(), Loader=yaml.FullLoader)
+        print(cfg)
+        cfg = SimpleNamespace(**cfg)  # easier dict access by dot, instead of ['']
+
+    tracker = RaceTracker(
+        args.reid_model,
+        cfg,
+        participants={bib.lower(): name for bib, name in race_config['participants'].items()},
+        device=args.device
+    )
+    tracker.display_delegate = lambda img: display_window.img_queue.put(img, block=False)
+
+    def overlay_annotations(frame, frame_num):
+        frame_annotations = track_boxes.get(frame_num, None)
+        frame_boxes = detection_boxes.get(frame_num, None)
+
+        if frame_boxes is not None:
+            boxes = frame_boxes['boxes']
+            kpts = frame_boxes['kpts']
+            crossings = frame_boxes['crossings']
+            frame = draw_annotation(img=frame, boxes=boxes, keypoints=kpts, crossings=crossings, labels=None,
+                                    conf=boxes[:, 4],
+                                    kpt_radius=2, colors=[(0, 255, 0)] * len(boxes), line_width=1)
+        if frame_annotations is not None:
+            boxes = frame_annotations['boxes']
+            kpts = frame_annotations['kpts']
+            crossings = frame_annotations['crossings']
+            bibs = [format(box[4].astype(int), '02x') for box in boxes]
+            names = [race_config['participants'].get(bib.upper(), None) for bib in bibs]
+            names = [name.split(" ")[0] if name else bib for bib, name in zip(bibs, names)]
+            labels = [f"{bib}{' ' + name if name else ''}" for bib, name in zip(bibs, names)]
+            frame = draw_annotation(img=frame, boxes=boxes, keypoints=kpts, crossings=crossings, labels=labels,
+                                    kpt_radius=2, line_width=1)
+
+        return frame
+
+    player = VideoPlayer(args.source, True)
+    player.overlay_delegate = overlay_annotations
+    if args.seek_frame:
+        args.seek_time = str(Timecode(player.get_current_time().framerate, frames=args.seek_timecode_frame))
+    if args.seek_time:
+        player.seek_to_time(args.seek_time)
+
+    runners = {}
+    finish_line_p0 = race_config['finish_line'][0]
+    finish_line_p1 = race_config['finish_line'][1]
+    # HACK(nickswalker 6-9-24): Patching a crop into the detector will be a mess and likely break in future versions.
+    # Instead, we will crop using ffmpeg before running the detector. We'll persist the cropped file in a predictable
+    # location, but first invocation with a new crop will be very expensive.
+    original_source_dims = [get_video_height_width(source) for source in args.source]
+    if args.crop:
+        args.imgsz = args.crop[0], args.crop[1]
+        # Apply crop to finish line
+        finish_line_p0 = (finish_line_p0[0] - args.crop[2], finish_line_p0[1] - args.crop[3])
+        finish_line_p1 = (finish_line_p1[0] - args.crop[2], finish_line_p1[1] - args.crop[3])
+
+        args.source = crop_videos(args.source, args.crop)
+
+    detection_boxes = load_annotations(args.project / 'detections')
+    (args.project / 'tracks').mkdir(exist_ok=True, parents=True)
+    track_boxes = load_annotations(args.project / 'tracks')
+
+    for frame_num, detections in detection_boxes.items():
+        player.seek_to_frame(frame_num)
+        frame = player._advance_frame()
+
+        boxes = Boxes(detections["boxes"], frame.shape[:2])
+        tracks = tracker.update(boxes, frame, frame_num)
+
+        # The last column of the tracks array is the original index of the detection. Rearrange them to match
+        idx = tracks[:, -1].astype(int)
+        boxes = Boxes(tracks[idx, :-1], frame.shape[:2])
+        crossings = [False] * len(boxes)
+        keypoints = None
+        track_boxes[frame_num] = {"boxes": boxes.data, "kpts": keypoints, "crossings": crossings}
+        save_txt_annotation(boxes, detections["kpts"], detections["crossings"], args.project / "tracks", replace=True)
+        display_window.img_queue.put(overlay_annotations(frame, frame_num))
+
+
+def parse_opt():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('project', type=pathlib.Path,
+                        help='save results to project/name')
+    parser.add_argument('--reid-model', type=Path, default='osnet_x0_25_msmt17.pt',
+                        help='reid model path')
+    parser.add_argument('--tracking-method', type=str, default='deepocsort',
+                        help='deepocsort, botsort, strongsort, ocsort, bytetrack')
+    # We depend on video files with timecode metadata. Hacking required to support other sources.
+    parser.add_argument('--source', type=pathlib.Path, nargs='+',
+                        help='filepath(s)')
+    parser.add_argument('--crop', nargs='+', type=int, default=None,
+                        help='inference area w h x y')
+    parser.add_argument('--conf', type=float, default=0.5,
+                        help='confidence threshold')
+    parser.add_argument('--iou', type=float, default=0.5,
+                        help='intersection over union (IoU) threshold for NMS')
+    parser.add_argument('--device', default='cuda',
+                        help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--show', action='store_true',
+                        help='display tracking video results')
+    parser.add_argument('--half', action='store_true',
+                        help='use FP16 half-precision inference')
+    parser.add_argument('--show-labels', action='store_false',
+                        help='either show all or only bboxes')
+    parser.add_argument('--show-conf', action='store_false',
+                        help='hide confidences when show')
+    parser.add_argument('--show-trajectories', action='store_true',
+                        help='show confidences')
+    parser.add_argument('--line-width', default=None, type=int,
+                        help='The line width of the bounding boxes. If None, it is scaled to the image size.')
+    parser.add_argument('--seek-frame', type=int,
+                        help='seek frame (index from start) to start tracking')
+    parser.add_argument('--seek-timecode-frame', type=int,
+                        help='seek frame (timecode frame index from start) to start tracking')
+    parser.add_argument('--seek-time', type=str, default=None, help='seek time to start tracking')
+    parser.add_argument('--continue-exp', default=False, action='store_true',
+                        help='continue tracking from last frame')
+
+    opt = parser.parse_args()
+    assert opt.seek_frame is None or opt.seek_timecode_frame is None, "Cannot set both seek_frame and seek_timecode_frame"
+    return opt
+
+
+if __name__ == "__main__":
+    opt = parse_opt()
+    run(opt)
--- visual_race_timing/tracker.py
+++ visual_race_timing/tracker.py
@@ -1,7 +1,11 @@
 import numpy as np
+from scipy.spatial.distance import cdist
+from ultralytics.engine.results import Boxes
 from ultralytics.trackers.basetrack import BaseTrack, TrackState
 from ultralytics.trackers.bot_sort import BOTrack, BOTSORT
 from ultralytics.trackers.utils import matching
+from ultralytics.utils import ops
+from ultralytics.utils.ops import xywh2ltwh
 from ultralytics.utils.plotting import Annotator, colors
 import torchreid
 import torch
@@ -9,6 +13,49 @@ import cv2
 import copy
 
 from visual_race_timing.prompts import ask_for_id
+
+
+def get_crops(xyxys, img, half=False, device='cuda'):
+    crops = []
+    h, w = img.shape[:2]
+    resize_dims = (128, 256)
+    interpolation_method = cv2.INTER_LINEAR
+    mean_array = np.array([0.485, 0.456, 0.406])
+    std_array = np.array([0.229, 0.224, 0.225])
+    # dets are of different sizes so batch preprocessing is not possible
+    for box in xyxys:
+        x1, y1, x2, y2 = box.astype('int')
+        x1 = max(0, x1)
+        y1 = max(0, y1)
+        x2 = min(w - 1, x2)
+        y2 = min(h - 1, y2)
+        crop = img[y1:y2, x1:x2]
+        # resize
+        crop = cv2.resize(
+            crop,
+            resize_dims,  # from (x, y) to (128, 256) | (w, h)
+            interpolation=interpolation_method,
+        )
+
+        # (cv2) BGR 2 (PIL) RGB. The ReID models have been trained with this channel order
+        crop = cv2.cvtColor(crop, cv2.COLOR_BGR2RGB)
+
+        crop = torch.from_numpy(crop).float()
+        crops.append(crop)
+
+    # List of torch tensor crops to unified torch tensor
+    crops = torch.stack(crops, dim=0)
+
+    # Normalize the batch
+    crops = crops / 255.0
+
+    # Standardize the batch
+    crops = (crops - mean_array) / std_array
+
+    crops = torch.permute(crops, (0, 3, 1, 2))
+    crops = crops.to(dtype=torch.half if half else torch.float, device=device)
+
+    return crops
 
 
 class RaceTracker(BOTSORT):
@@ -25,16 +72,29 @@ class RaceTracker(BOTSORT):
         self.device = device
         self.unknowns = {}
         self.participants = participants
+        self.display_delegate = lambda x: None
         if args.with_reid:
             self.feature_extractor = torchreid.utils.FeatureExtractor(model_name='osnet_ain_x1_0',
                                                                       model_path=model_weights,
                                                                       device=self.device)
         # HACK: Monkey patch so we get hex print outs to match bibs
         BOTrack.__str__ =  lambda self: f"OT_{format(self.track_id, '02x')}_({self.start_frame}-{self.end_frame})"
+        # cv2.namedWindow("Decision")
+        self.frame_id = None
 
-    def update(self, results, img=None):
+    def update(self, results, img=None, frame_id=None):
         """Updates object tracker with new detections and returns tracked object bounding boxes."""
-        self.frame_id += 1
+        if self.frame_id is None and frame_id is not None:
+            self.frame_id = frame_id
+        elif frame_id is not None:
+            delta = frame_id - self.frame_id
+            if delta > 1:
+                # We've skipped some frames, so we need null updates
+                for i in range(delta - 1):
+                    self.update(Boxes(np.zeros((0, 7)), img.shape[:2]), img)
+            return self.update(results, img)
+        else:
+            self.frame_id += 1
         activated_stracks = [] # Active and detected within this update
         refind_stracks = []
         lost_stracks = [] # Temporarily unmatched, but could come back momentarily
@@ -52,14 +112,15 @@ class RaceTracker(BOTSORT):
         dets = bboxes[remain_inds]
         xyxys = xyxys[remain_inds]
 
-        if len(xyxys > 0):
-            crops = self.get_crops(xyxys, img)
+        if self.feature_extractor and len(xyxys > 0):
+            crops = get_crops(xyxys, img)
             features = self.feature_extractor(crops)
             features = [x.cpu().numpy() for x in features]
         else:
             features = []
 
-        detections = [BOTrack(xyxy, s, c, f) for (xyxy, s, c, f) in zip(dets, scores, cls, features)]
+        # NOTE: BOTrack constructor's label for first arg is wrong. It's treated as xywh, not tlwh
+        detections = [BOTrack(xywh, s, c, f) for (xywh, s, c, f) in zip(dets, scores, cls, features)]
 
         # Step 1: First association, with high score detection boxes
         strack_pool = self.joint_stracks(self.tracked_stracks, self.lost_stracks)
@@ -82,8 +143,7 @@ class RaceTracker(BOTSORT):
                 copy.deepcopy(img),
             )
             annotator.box_label(detections[inew].xyxy, label=f"{scores[inew]:.2f} {_ + 1}/{len(u_detection)}", color=colors(0))
-            cv2.imshow("Race Tracking", annotator.result())
-            cv2.waitKey(1)
+            self.display_delegate(annotator.result())
             if len(strack_pool) == 0:
                 # No one to match with, new track
                 user_input = ask_for_id([(bib.lower(), (name,)) for bib, name in self.participants.items()], allow_other=True)
@@ -172,7 +232,146 @@ class RaceTracker(BOTSORT):
                 track.mark_removed()
                 removed_stracks.append(track)
 
+        self.tracked_stracks = [t for t in self.tracked_stracks if t.state == TrackState.Tracked]
+        self.tracked_stracks = self.joint_stracks(self.tracked_stracks, activated_stracks)
+        self.tracked_stracks = self.joint_stracks(self.tracked_stracks, refind_stracks)
+        self.lost_stracks = self.sub_stracks(self.lost_stracks, self.tracked_stracks)
+        self.lost_stracks = self.sub_stracks(self.lost_stracks, self.removed_stracks)
+        self.lost_stracks.extend(lost_stracks)
+        self.removed_stracks = self.sub_stracks(self.removed_stracks, self.tracked_stracks)
+        self.removed_stracks.extend(removed_stracks)
 
+        return np.asarray([x.result for x in self.tracked_stracks], dtype=np.float32)
+
+    def get_dists(self, tracks, detections):
+        """Get distances between tracks and detections using IoU and (optionally) ReID embeddings."""
+        dists = matching.iou_distance(tracks, detections)
+        dists_mask = dists > self.proximity_thresh
+
+        # Just multiplies together detection confidence and IoU
+        dists = matching.fuse_score(dists, detections)
+
+        if self.args.with_reid:
+            emb_dists = matching.embedding_distance(tracks, detections) / 2.0
+            emb_dists[emb_dists > self.appearance_thresh] = 1.0
+            # emb_dists[dists_mask] = 1.0
+            dists = np.minimum(dists, emb_dists)
+        return dists
+
+
+class PartiallySupervisedTracker(BOTSORT):
+    def __init__(
+            self,
+            model_weights,
+            args,
+            frame_rate=30,
+            device="0"
+    ):
+        super().__init__(args, frame_rate)
+        self.half = False
+        self.device = device
+        self.display_delegate = lambda x: None
+        if args.with_reid:
+            self.feature_extractor = torchreid.utils.FeatureExtractor(model_name='osnet_ain_x1_0',
+                                                                      model_path=model_weights,
+                                                                      device=self.device)
+        # HACK: Monkey patch so we get hex print outs to match bibs
+        BOTrack.__str__ = lambda self: f"OT_{format(self.track_id, '02x')}_({self.start_frame}-{self.end_frame})"
+        # cv2.namedWindow("Decision")
+
+    def update(self, results, img=None):
+        """Updates object tracker with new detections and returns tracked object bounding boxes."""
+        self.frame_id += 1
+        activated_stracks = []  # Active and detected within this update
+        refind_stracks = []
+        lost_stracks = []  # Temporarily unmatched, but could come back momentarily
+        removed_stracks = []  # Assumed gone. Requires human intervention to re-activate
+
+        scores = results.conf
+        bboxes = results.xywhr if hasattr(results, "xywhr") else results.xywh
+        xyxys = results.xyxy
+        if results.id is not None:
+            box_ids = results.id
+        else:
+            box_ids = np.full((len(bboxes)), -1)
+        # Add index
+        bboxes = np.concatenate([bboxes, np.arange(len(bboxes)).reshape(-1, 1)], axis=-1)
+        cls = results.cls
+
+            # (cv2) BGR 2 (PIL) RGB. The ReID models have been trained with this channel order
+            crop = cv2.cvtColor(crop, cv2.COLOR_BGR2RGB)
+
+            crop = torch.from_numpy(crop).float()
+            crops.append(crop)
+
+        # List of torch tensor crops to unified torch tensor
+        crops = torch.stack(crops, dim=0)
+
+        # NOTE: BOTrack constructor's label for first arg is wrong. It's treated as xywh, not tlwh
+        detections = [BOTrack(xywh, s, c, f) for (xywh, s, c, f) in zip(dets, scores, cls, features)]
+
+        crops = torch.permute(crops, (0, 3, 1, 2))
+        crops = crops.to(dtype=torch.half if self.half else torch.float, device=self.device)
+
+        matches, u_track, u_detection = matching.linear_assignment(dists, thresh=self.args.match_thresh)
+        emb_dists = matching.embedding_distance(strack_pool, detections)
+
+        # Step 2: Deal with the unmatched
+        track_ids = [t.track_id for t in strack_pool]
+        for _, inew in enumerate(u_detection):
+            # First time we're seeing this ID, it didn't match with anything. Make a new track
+            if box_ids[inew] != -1:
+                det = detections[inew]
+                det.activate(self.kalman_filter, self.frame_id)
+
+                det.track_id = box_ids[inew]
+                activated_stracks.append(det)
+                continue
+
+            # FIXME: Should make a guess at the matching based on reid model too, but should figure out
+            #   how to share the representation with the outside too
+            matched_tracks = [match[0] for match in matches]
+            # Mask embedding dist for matched tracks
+            emb_dists[matched_tracks] = 1
+            # Get (at most) the 5 closest embeddings
+            emb_dist_ranking = np.argsort(emb_dists[:, inew])
+            match_emb_dists = emb_dists[emb_dist_ranking, inew]
+            det = detections[inew]
+            closest_tracks = [strack_pool[i] for i in emb_dist_ranking[:5]]
+            dists = list(match_emb_dists[:5])
+
+            # First time a person is tracked
+            det.activate(self.kalman_filter, self.frame_id)
+            activated_stracks.append(det)
+            det.track_id += 0xF00  # Blow this ID out of the race namespace so it doesn't accidentally get treated as a real bib ID
+
+        # Step 3: Update tracks based on matched pairs
+        for itracked, idet in matches:
+            track = strack_pool[itracked]
+            det = detections[idet]
+            if box_ids[idet] != -1:
+                # This track got matched to a detection with a known ID. Adopt the ID
+                track.track_id = box_ids[idet]
+            if track.state == TrackState.Tracked:
+                track.update(det, self.frame_id)
+                activated_stracks.append(track)
+            else:
+                track.re_activate(det, self.frame_id, new_id=False)
+                refind_stracks.append(track)
+
+        # Step 4: Update state
+        # Promote to lost if not matched
+        unmatched_tracks = [strack_pool[i] for i in u_track if strack_pool[i].state == TrackState.Tracked]
+        for it in range(len(unmatched_tracks)):
+            track = unmatched_tracks[it]
+            if track.state != TrackState.Lost:
+                track.mark_lost()
+                lost_stracks.append(track)
+        # Promote to removed if lost for too long
+        for track in self.lost_stracks:
+            if self.frame_id - track.end_frame > self.max_time_lost:
+                track.mark_removed()
+                removed_stracks.append(track)
 
         self.tracked_stracks = [t for t in self.tracked_stracks if t.state == TrackState.Tracked]
         self.tracked_stracks = self.joint_stracks(self.tracked_stracks, activated_stracks)
@@ -185,47 +384,36 @@ class RaceTracker(BOTSORT):
 
         return np.asarray([x.result for x in self.tracked_stracks], dtype=np.float32)
 
-    def get_crops(self, xyxys, img):
-        crops = []
-        h, w = img.shape[:2]
-        resize_dims = (128, 256)
-        interpolation_method = cv2.INTER_LINEAR
-        mean_array = np.array([0.485, 0.456, 0.406])
-        std_array = np.array([0.229, 0.224, 0.225])
-        # dets are of different sizes so batch preprocessing is not possible
-        for box in xyxys:
-            x1, y1, x2, y2 = box.astype('int')
-            x1 = max(0, x1)
-            y1 = max(0, y1)
-            x2 = min(w - 1, x2)
-            y2 = min(h - 1, y2)
-            crop = img[y1:y2, x1:x2]
-            # resize
-            crop = cv2.resize(
-                crop,
-                resize_dims,  # from (x, y) to (128, 256) | (w, h)
-                interpolation=interpolation_method,
-            )
+    def guess_id(self, frame, box):
+        crops = get_crops(box[:, :4], frame)
+        query_features = self.feature_extractor(crops).cpu().numpy()[0]
+        strack_pool = self.joint_stracks(self.tracked_stracks, self.lost_stracks)
+        strack_pool = self.joint_stracks(strack_pool, self.removed_stracks)
+        candidate_features_by_id = {track.track_id: track.smooth_feat for track in strack_pool}
+        candidate_features = np.asarray(list(candidate_features_by_id.values()), dtype=np.float32)
+        cost_matrix = np.zeros((len(candidate_features), 1), dtype=np.float32)
+        if cost_matrix.size == 0:
+            return [], cost_matrix
+        det_features = np.asarray([query_features], dtype=np.float32)
+        cost_matrix = np.maximum(0.0, cdist(candidate_features, det_features, 'cosine'))  # Normalized features
+        return list(candidate_features_by_id.keys()), cost_matrix
 
-            # (cv2) BGR 2 (PIL) RGB. The ReID models have been trained with this channel order
-            crop = cv2.cvtColor(crop, cv2.COLOR_BGR2RGB)
-
-            crop = torch.from_numpy(crop).float()
-            crops.append(crop)
-
-        # List of torch tensor crops to unified torch tensor
-        crops = torch.stack(crops, dim=0)
-
-        # Normalize the batch
-        crops = crops / 255.0
-
-        # Standardize the batch
-        crops = (crops - mean_array) / std_array
-
-        crops = torch.permute(crops, (0, 3, 1, 2))
-        crops = crops.to(dtype=torch.half if self.half else torch.float, device=self.device)
-
-        return crops
+    def update_participant_features(self, frame, box, track_id: int):
+        # One box at a time!
+        crops = get_crops(box[:, :4], frame)
+        query_features = self.feature_extractor(crops).cpu().numpy()[0]
+        strack_pool = self.joint_stracks(self.tracked_stracks, self.lost_stracks)
+        strack_pool = self.joint_stracks(strack_pool, self.removed_stracks)
+        for track in strack_pool:
+            if track.track_id == track_id:
+                track.update_features(query_features)
+                return
+        reformated_box = box.copy()
+        reformated_box[:, :4] = ops.xywh2xyxy(reformated_box[:, :4])
+        # There's no track yet, make one
+        fresh_track = BOTrack(reformated_box.squeeze()[:5], 1, 0, query_features)
+        fresh_track.track_id = track_id
+        self.removed_stracks.append(fresh_track)
 
     def get_dists(self, tracks, detections):
         """Get distances between tracks and detections using IoU and (optionally) ReID embeddings."""
